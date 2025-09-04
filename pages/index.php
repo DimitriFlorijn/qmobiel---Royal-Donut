b0c58b208519dbf23d4c9a6a503ef4747e95450a
DimitriFlorijn <117824651+DimitriFlorijn@users.noreply.github.com>
2025-09-04 13:19:28 +0200

    header made with style
    added header  with .css
    added Logo

---
+++ pages/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>homescreen</title>
+  <link rel="stylesheet" href="../styling/main.css">
+</head>
+<body>
+  <header>
+    <img src="../img/RDLogo.png" alt="RDLogo">
+    <h1>Welcome chef : RoyalDonut Kitchen</h1>
+  </header>
+</body>
+</html>
