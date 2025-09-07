a71fe73091c891c1c814ee69c64b5de40786be13
DimitriFlorijn <117824651+DimitriFlorijn@users.noreply.github.com>
2025-09-07 18:28:13 +0200

    did some styling for the page
    styling for donuts to the left and search top right, and add a donut bottom right
    
    also removed a if statment from conn.php

--- backend/conn.php
+++ backend/conn.php
@@ -5,6 +5,3 @@ require_once 'config.php';
 $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($conn->connect_error) {
-  die("Verbinding mislukt: " . $conn->connect_error);
-}
